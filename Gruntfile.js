b3b189af5ba05d7248c2aeca495f8a560e155bac
lezed1 <lezed1@gmail.com>
2013-05-14 19:25:24 -0400

    Allow non-local connections to dev server

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function (grunt) {
       options: {
         port: process.env.PORT || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: process.env.HOST || 'localhost'
+        hostname: process.env.HOST || '0.0.0.0'
       },
       livereload: {
         options: {
