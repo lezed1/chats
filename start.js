bf6c436afb453e3155da2f66b4cf00e7f493647b
lezed1 <lezed1@gmail.com>
2013-05-17 23:22:31 -0400

    Fixed Jade compile error

--- start.js
+++ start.js
@@ -1,5 +1,8 @@
 express = require('express')
 dist_server = require('./express.js')
+port = process.env.PORT || 9000
 
 dist_server.use(express.static('dist/'))
-dist_server.listen(process.env.PORT || 9000)
+dist_server.listen(port)
+
+console.log('Server running on port: ' + port)
