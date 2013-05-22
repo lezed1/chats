820133fcbcc5b781667f7fe1c9e1f6dd7daad7ff
lezed1 <lezed1@gmail.com>
2013-05-21 23:27:38 -0400

    Mobile navbar now collaspes

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,11 +28,11 @@ module.exports = function (grunt) {
         tasks: ['coffee:dist']
       },
       jade: {
-        files: '<%= yeoman.app %>/{,*/}*.jade',
+        files: '<%= yeoman.app %>/Public/{,*/}*.jade',
         tasks: ['jade']
       },
       stylus: {
-        files: '<%= yeoman.app %>/{,*/}*.styl',
+        files: '<%= yeoman.app %>/Public/{,*/}*.styl',
         tasks: ['stylus']
       },
       coffeeTest: {
